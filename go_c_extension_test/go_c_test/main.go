8e0deaa0793b825b60829fedc0e7994d95c10815
John Lau <johnlau@umail.ucsb.edu>
2018-04-20 11:51:49 -0700

    Fixed file structure

--- go_c_extension_test/go_c_test/main.go
+++ go_c_extension_test/go_c_test/main.go
@@ -2,14 +2,13 @@ package main
 
 /*
 This code was found from here: https://github.com/AlekSi/cgo-by-example/blob/master/main.go
-The main purpose of this file is for us to play around with calling 
+The purpose of this file is for us to play around with calling
 C from Go and seeing what the process is like
 */
 
-
 /*
 #include "lib/lib.h"
-#include <stdlib.h>      
+#include <stdlib.h>
 #cgo LDFLAGS: -L. -llib
 // simple wrapper, see below
 void call_f5_with_f1(void) {
