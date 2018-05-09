4f0a795cde907b3a61a85ae5d7a7a8ef17292db0
Rey Tang <tang.changrey@gmail.com>
2018-05-09 14:57:21 -0700

    Added in c extension for spectral norm in go

---
+++ spectral_norm/go_c_ext/lib/lib.h
@@ -0,0 +1,2 @@
+void eval_AtA_times_u(int N, const double u[], double AtAu[]);
+
---
+++ spectral_norm/go_c_ext/test.go
@@ -0,0 +1,50 @@
+package main
+
+/*
+#include "lib/lib.h"
+#include <stdlib.h>
+#cgo LDFLAGS: -L. -llib
+*/
+import "C"
+
+import (
+	"flag"
+	"fmt"
+	"math"
+	"strconv"
+	"unsafe"
+)
+
+func main() {
+	flag.Parse()
+	n := 10
+	if flag.NArg() > 0 {
+		n, _ = strconv.Atoi(flag.Arg(0))
+	}
+
+	N := C.int(n)
+
+	u := (C.malloc(C.size_t(N) * C.size_t(unsafe.Sizeof(C.double(0)))))
+	u_slice := (*[1<<30 - 1]C.double)(u)[:n:n]
+
+	for i := 0; i < n; i++ {
+		u_slice[i] = C.double(1.0)
+	}
+
+	v := (C.malloc(C.size_t(N) * C.size_t(unsafe.Sizeof(C.double(0)))))
+	v_slice := (*[1<<30 - 1]C.double)(v)[:n:n]
+
+	for i := 0; i < 10; i++ {
+		C.eval_AtA_times_u(N, (*C.double)(u), (*C.double)(v))
+		C.eval_AtA_times_u(N, (*C.double)(v), (*C.double)(u))
+		// v.ATimesTransp(u)
+		// u.ATimesTransp(v)
+	}
+	var vBv, vv float64
+	for i := 0; i < n; i++ {
+		vBv += float64(u_slice[i]) * float64(v_slice[i])
+		vv += float64(v_slice[i]) * float64(v_slice[i])
+	}
+	fmt.Printf("%0.9f\n", math.Sqrt(vBv/vv))
+}
+
